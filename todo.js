6a1c86d47fd735d9e7f315b3653641937417f7c6
froggy <chrisdetmering@gmail.com>
2021-03-31 14:32:09 -0700

    Andy review complete

--- todo.js
+++ todo.js
@@ -1,58 +1,60 @@
-const button = document.getElementById('addButton');
-button.addEventListener('click', addItem)
 
-const text = document.getElementById("textContent").addEventListener("keyup", function (e) {
-    if (e.keyCode === 13) {
-        addItem();
-    }
-});
+document.getElementById('addButton')
+.addEventListener('click', displayTodo)
 
-function addItem(e) {
+
+function displayTodo(e) {
     e.preventDefault();
 
-    var myList = document.getElementById("myList");
-    var textContent = document.getElementById('textContent');
-    var li = document.createElement("li");
+    const myList = document.getElementById("myList");
+    const textContent = document.getElementById('textContent');
+    const text = textContent.value; 
+    const todo = document.createElement("li");
+    const removeButton = createRemoveButton(todo); 
+    const toggleTodoButton = createToggleTodo(todo); 
 
-    li.setAttribute('id', textContent.value);
-    li.appendChild(document.createTextNode(textContent.value));
+    todo.appendChild(removeButton);
+    todo.appendChild(toggleTodoButton);
+    todo.appendChild(document.createTextNode(textContent.value));
+    myList.appendChild(todo);
+    textContent.value = '';
+}
 
-    var remove = document.createElement("button");
+
+function createRemoveButton(li) { 
+    const remove = document.createElement("button");
     remove.innerText = "Delete";
     remove.style.borderRadius = "10px";
     remove.style.float = "right";
     remove.style.height = "30px";
-    remove.style.fontFamily = "Chalkboard, fantasy"
-    remove.style.fontSize = "large"
-    remove.style.backgroundColor = "red"
-    remove.style.color = "white"
+    remove.style.fontFamily = "Chalkboard, fantasy";
+    remove.style.fontSize = "large";
+    remove.style.backgroundColor = "red";
+    remove.style.color = "white";
 
-    li.appendChild(remove);
+    remove.addEventListener("click", () => {
+        li.remove();
+    })
 
-    function removeItem(e) {
-        myList.removeChild(li);
-    };
+    return remove; 
+}
 
-    var done = document.createElement("button");
+
+function createToggleTodo(li) { 
+    const done = document.createElement("button");
     done.innerText = "Complete";
     done.style.borderRadius = "10px";
     done.style.height = "30px";
     done.style.float = "right";
-    done.style.fontFamily = "Chalkboard, fantasy"
-    done.style.fontSize = "large"
-    done.style.backgroundColor = "rgb(18, 90, 18)"
-    done.style.color = "white"
-
-
-    li.appendChild(done);
-
-    function completed(e) {
+    done.style.fontFamily = "Chalkboard, fantasy";
+    done.style.fontSize = "large";
+    done.style.backgroundColor = "rgb(18, 90, 18)";
+    done.style.color = "white";
+    
+    done.addEventListener("click", () => {
         li.style.textDecoration = "line-through";
         li.style.color = "rgb(18, 90, 18)";
-    };
+    })
 
-    remove.addEventListener("click", removeItem)
-    done.addEventListener("click", completed)
-
-    myList.appendChild(li);
+    return done; 
 }
